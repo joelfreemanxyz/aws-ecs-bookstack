d49236438073ce3a2d041055beee29d71e318b51
Joel Freeman <joel.freeman@tribe.co.nz>
2019-12-13 14:47:54 +1300

    Added security groups, and ALB to terraform.

--- aws-poc/terraform/security.tf
+++ aws-poc/terraform/security.tf
@@ -1,15 +1,39 @@
 resource "aws_security_group" "wikijs_alb_sg" {
-  name = "wikijs_alb_sg"
-  description = "Controll access to the ELB"
+  name        = "wikijs_alb_sg"
+  description = "Control access to the ELB"
+  vpc_id      = "${aws_vpc.wikijs_vpc.id}"
+}
+# Allow incoming HTTP requests to load balancer
+resource "aws_security_group_rule" "wikijs_alb_sg_allow_incoming_http" {
+  type              = "ingress"
+  from_port         = "80"
+  to_port           = "80"
+  protocol          = "tcp"
+  vpc_id            = "${aws_vpc.wikijs_vpc.id}"
+  security_group_id = "${aws_security_group.wikijs_alb_sg.id}"
+}
+# Allow incoming HTTPS requests to load balancer
+resource "aws_security_group_rule" "wikijs_alb_sg_allow_incoming_https" {
+  type              = "ingress"
+  from_port         = "443"
+  to_port           = "443"
+  protocol          = "tcp"
+  vpc_id            = "${aws_vpc.wikijs_vpc.id}"
+  security_group_id = "${aws_security_group.wikijs_alb_sg.id}"
 }
 
 resource "aws_security_group" "wikijs_ecs_tasks_sg" {
-  name = "wikijs_ecs_tasks_sg"
+  name        = "wikijs_ecs_tasks_sg"
   description = "Allow inbound access to tasks from the ALB ONLY."
+  vpc_id      = "${aws_vpc.wikijs_vpc.id}"
 }
-
-resource "aws_security_group" "wikijs_rds_sg" {
-  name = "wikijs_rds_sg"
-  description = "Allow inbound access from ECS ONLY."
+# Allow incoming requests on port 3000 from the load balancer.
+resource "aws_security_group_rule" "wikijs_ecs_tasks_sg_allow_incoming_3000" {
+  type                    = "ingress"
+  from_port               = "3000"
+  to_port                 = "3000"
+  protocol                = "tcp"
+  vpc_id                  = "${aws_vpc.wikijs_vpc.id}"
+  security_group_id       = "${aws_security_group.wikijs_ecs_tasks_sg.id}"
+  source_security_group_id = "${aws_security_group.wikijs_alb_sg.aw}"
 }
-
